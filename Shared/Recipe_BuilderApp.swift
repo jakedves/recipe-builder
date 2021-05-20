92c0871d1d8ad3cad8a5a5bb7d1dcdcf3473c421
Jake <jakedves@gmail.com>
2021-05-20 21:08:49 +0100

    initial commit

---
+++ Shared/Recipe_BuilderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_BuilderApp.swift
+//  Shared
+//
+//  Created by Jake Davies on 20/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_BuilderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
