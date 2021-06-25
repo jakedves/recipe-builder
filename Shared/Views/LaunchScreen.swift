93a7ca7494d42a328bbb9f78c57f913ca51d9c39
Jake <jakedves@gmail.com>
2021-06-25 20:41:08 +0100

    Added logo to launch screen

--- Shared/Views/LaunchScreen.swift
+++ Shared/Views/LaunchScreen.swift
@@ -12,29 +12,34 @@ struct LaunchScreen: View {
     @State var buttonPressed = false
     let title = "WELCOME"
     let subtitle = "What's cooking?"
-    let titleSize = CGFloat(70)
-    let messageSize = CGFloat(24)
+    let titleSize = CGFloat(50)
+    let messageSize = CGFloat(18)
     let themeColor = Color.green
 
     var body: some View {
             // Above Image Layer
             VStack {
                 
-                Spacer().frame(height: 90)
+                Spacer().frame(height: 250)
                 
                 // Text
-                VStack {
-                    Text(title)
-                        .font(.system(size: titleSize))
-                        .fontWeight(.heavy)
-                        .foregroundColor(themeColor)
-                        .contrast(0.8)
-                    Text(subtitle)
-                        .foregroundColor(themeColor)
-                        .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
-                        .font(.system(size: messageSize))
+                HStack {
+                    Image("Logo")
+                        .resizable()
+                        .frame(width: 65, height: 65)
+                    
+                    VStack {
+                        Text(title)
+                            .font(.system(size: titleSize))
+                            .fontWeight(.heavy)
+                            .foregroundColor(themeColor)
+                            .contrast(0.8)
+                        Text(subtitle)
+                            .foregroundColor(themeColor)
+                            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                            .font(.system(size: messageSize))
+                    }.padding(.top, 5)
                 }
-                .padding(.top, 150)
                 
                 Spacer().frame(height: 80)
                 
